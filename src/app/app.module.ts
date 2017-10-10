81dd0e5567061f299b50e9719d6df3b74e8fb1dd
giacomo antonelli <giacant@gmail.com>
2017-10-10 09:39:55 -0500

    Million dollar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { HttpModule } from '@angular/http';
+
+import { MLApp } from './app.component';
+import { InitPage } from '../pages/init/init';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+
+import { AutenticazioneProvider } from '../providers/autenticazione/autenticazione';
+import { DatiPubbliciProvider } from '../providers/dati-pubblici/dati-pubblici';
 
 @NgModule({
   declarations: [
-    MyApp,
+    MLApp,
     HomePage,
-    ListPage
+    LoginPage,
+    InitPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MLApp),
+    IonicStorageModule.forRoot(),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    MLApp,
     HomePage,
-    ListPage
+    LoginPage,
+    InitPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AutenticazioneProvider,
+    DatiPubbliciProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { AutenticazioneProvider } from '../../providers/autenticazione/autenticazione';
+import { DatiPubbliciProvider } from '../../providers/dati-pubblici/dati-pubblici';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  utente;
+  squadre;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public autenticazione:AutenticazioneProvider, public datiPubblici:DatiPubbliciProvider, public navCtrl: NavController, public navParams: NavParams) {
+    this.utente=this.autenticazione.utente;
+    this.squadre=this.autenticazione.squadre;
   }
 
+  ionViewCanEnter(){
+    return this.autenticazione.loggato;
+  }
 }
